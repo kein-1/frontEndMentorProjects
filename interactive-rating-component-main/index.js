68e576a1733fdb1edd9d17b0f108accb61c53801
Kevin J.H Li <tensaikev@gmail.com>
2022-08-04 19:16:19 -0400

    projects

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -1,11 +1,49 @@
 
-const ratingBox = document.getElementById("ratings-container")
+const ratingBox = document.getElementsByClassName("ratings-container")
+const message = document.getElementById("message")
+const submitBtn = document.getElementById("submit-btn")
+const starIcon = document.getElementById("star-icon")
 
-for (let i = 0; i < 5; i++){
+let statusBar = false
+let empty =""
+submitBtn.onclick = () => {
+
+    starIcon.remove()
+    statusBar = true
+    if (statusBar == true && empty.length > 0){
+        // console.log(`You selected ${empty} out of 5`)
+        let newPic = document.createElement("img")
+        let congratsText = document.createElement("p")
+
+
+        newPic.setAttribute("class", "newPic")
+        newPic.src = "./images/illustration-thank-you.svg"
+        
+        congratsText.innerText = `You selected ${empty} out of 5`
+
+        while (message.firstChild){
+            message.removeChild(message.lastChild)
+        }
+        
+        message.appendChild(newPic)
+        message.append(congratsText)
+    }
+
+}
+
+
+
+for (let i = 1; i <= 5; i++){
     let newRating = document.createElement("button")
     newRating.setAttribute("class", "ratingButton")
     newRating.setAttribute("id", "ratingButton")
-    // newRating.addEventListener("click",())
+    newRating.addEventListener("click",() => {
+    empty+=i.toString()
+    console.log(empty)
+    }
+    )
+
     newRating.innerText = i
-    ratingBox.appendChild(newRating)
+    ratingBox[0].appendChild(newRating)
 }
+
